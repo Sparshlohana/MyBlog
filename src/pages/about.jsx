759c14a943c8bf7aa817d3e81e6bf4676c4ce306
Sparshlohana <sparsh.lohana@gmail.com>
2023-04-05 12:30:26 +0530

    About Us Page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,109 @@
-import React from "react";
+import styles from "../styles/About.module.css";
 
 const About = () => {
-  return <div>I am about Component</div>;
+  return (
+    <>
+      <div className={styles.aboutContainerMain}>
+        <div className={styles.aboutHeadingContainer}>
+          <h1 className={styles.aboutHeading}>About</h1>
+        </div>
+        <div className={styles.aboutServiceContainer}>
+          <p className={styles.aboutService}>
+            Hii, My name is Sparsh. Nice to meet you!!
+          </p>
+        </div>
+      </div>
+      <hr />
+      <div className={styles.aboutContactMeContainer}>
+        <h1 className={styles.aboutContactHeading}>Contact Me</h1>
+        <div className={styles.iconContainer}>
+          <a
+            href="https://api.whatsapp.com/send?phone=8154000250&text=Heyy%20Sparsh..%20Nice%20to%20meet%20you!!!"
+            className={styles.contactMeIconsAnchor}
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/479px-WhatsApp.svg.png"
+              alt="WPIcon"
+              className={styles.contactMeIcons}
+            />
+          </a>
+        </div>
+
+        <div className={styles.iconContainer}>
+          <a
+            href="https://www.instagram.com/its_.sparshiii/"
+            className={styles.contactMeIconsAnchor}
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"
+              alt="WPIcon"
+              className={styles.contactMeIcons}
+            />
+          </a>
+        </div>
+        <div className={styles.iconContainer}>
+          <a
+            href="https://www.linkedin.com/in/sparsh-r-lohana-845457242/"
+            className={styles.contactMeIconsAnchor}
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+              alt="WPIcon"
+              className={styles.contactMeIcons}
+            />
+          </a>
+        </div>
+        <div className={styles.iconContainer}>
+          <a
+            href="https://github.com/Sparshlohana"
+            className={styles.contactMeIconsAnchor}
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <img
+              src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png"
+              alt="WPIcon"
+              className={styles.contactMeIcons}
+            />
+          </a>
+        </div>
+        <div className={styles.iconContainer}>
+          <a
+            href="https://twitter.com/Sparsh_8304"
+            className={styles.contactMeIconsAnchor}
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/2491px-Twitter-logo.svg.png"
+              alt="WPIcon"
+              className={styles.contactMeIcons}
+            />
+          </a>
+        </div>
+        <div className={styles.iconContainer}>
+          <a
+            href="https://www.facebook.com/sparshr.lohana"
+            className={styles.contactMeIconsAnchor}
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/124/124010.png"
+              alt="WPIcon"
+              className={styles.contactMeIcons}
+            />
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
